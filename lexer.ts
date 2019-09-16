ff31292a33f9b84a5a4b304e457272bc12945651
barbara stall <barbara.stall1@marist.edu>
2019-09-16 18:50:41 -0400

    Create lexer.ts
    Commented out tasks.

---
+++ lexer.ts
@@ -0,0 +1,20 @@
+module StallCompiler{
+    export class lexer{
+        public lexerProgram(){
+            //error count
+
+            //keyword and symbol variable intialization
+
+            //regular expression variables
+
+            //implementing multiple programs at one execution
+
+            //get code, trim, split into lines, get length
+
+            //check for $ - end of program symbol
+
+            //loop through code
+            
+        }
+    }
+}
