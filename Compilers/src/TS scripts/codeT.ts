706d1b1615ac4b0f37bd162ef5eca5f7fde82d1f
barbara stall <barbara.stall1@marist.edu>
2019-12-02 19:38:22 -0500

    Starting methods for code table file.

--- Compilers/src/TS scripts/codeT.ts
+++ Compilers/src/TS scripts/codeT.ts
@@ -1,7 +1,37 @@
 //file to keep track of code generated to output to table
 module StallCompiler{
     export class CodeT{
-        
         public table: string[] = [];
+        private currentAddr: number = 0;
+        private heapPos: number = 255;
+        //only allowed 265 bytes
+        constructor() {
+            for (var i = 0; i < 256; i++) {
+                this.table[i] = "";
+            }
+        }
+        //adds to table, updates address
+        public addByte(byte: string): void {
+            this.table[this.currentAddr] = byte;
+            this.currentAddr++;
+        }
+        
+        //add to specific location
+        public addByteAtAddr(byte: string, address: string): void {
+            byte = byte.toUpperCase();
+            this.table[address] = byte;
+        }
+        
+        public getCurrentAddr(): number {
+            return this.currentAddr;
+        }
+        //makes the unused spaces 00's
+        public zeroOutEmptySlots(): void {
+            for (var i = 0; i < 256; i++) {
+                if (this.table[i] === "") {
+                    this.table[i] = "00";
+                }
+            }
+        }
     }
 }
