e78e4b8f2227ef93d53adac41e2c5af579609316
barbara stall <barbara.stall1@marist.edu>
2019-12-04 14:18:00 -0500

    Populating main static table method.

--- Compilers/src/TS scripts/staticT.ts
+++ Compilers/src/TS scripts/staticT.ts
@@ -1,23 +1,51 @@
 //file to keep track of static allocation of memory
 module StallCompiler{
     export class staticT{
+        private items: staticTItem[] = [];
+        private prefix: string = "T";
+        private suffix: number = 0;
+        
 
     }
 
     export class staticTItem{
 
-        private temp: string;
         private id: string;
+        private temp: string;
         private scope: number;
         private addr: number = 0;
         private type: string;
 
         constructor(temp: string, id: string, scope: number, address: number, type: string) {
-            this.temp = temp;
+            
             this.id = id;
+            this.temp = temp;
             this.scope = scope; 
             this.addr = address;
             this.type = type;
+        }
+
+        public getId(): string {
+            return this.id;
+        }
+        
+        public setId(id: string): void {
+            this.id = id;
+        }
+        public getTemp(): string {
+            return this.temp;
+        }
+        
+        public setTemp(temp: string): void {
+            this.temp = temp;
+        }
+        
+        public getAddr(): number {
+            return this.addr;
+        }
+        
+        public getType(): string {
+            return this.type;
         }
     }
 }
