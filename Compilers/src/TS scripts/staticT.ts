89ad6dc6b54380c2db1533cc8a993683361cdee2
barbara stall <barbara.stall1@marist.edu>
2019-12-04 13:53:46 -0500

    Initialize static t item variables and constructor.

--- Compilers/src/TS scripts/staticT.ts
+++ Compilers/src/TS scripts/staticT.ts
@@ -3,4 +3,21 @@ module StallCompiler{
     export class staticT{
 
     }
+
+    export class staticTItem{
+
+        private temp: string;
+        private id: string;
+        private scope: number;
+        private addr: number = 0;
+        private type: string;
+
+        constructor(temp: string, id: string, scope: number, address: number, type: string) {
+            this.temp = temp;
+            this.id = id;
+            this.scope = scope; 
+            this.addr = address;
+            this.type = type;
+        }
+    }
 }
