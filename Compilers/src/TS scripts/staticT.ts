4cd20a1501142f9190f195851741cf5a02863f12
barbara stall <barbara.stall1@marist.edu>
2019-12-11 13:21:53 -0500

    Populated static table file, basically the same as jump table.

--- Compilers/src/TS scripts/staticT.ts
+++ Compilers/src/TS scripts/staticT.ts
@@ -5,6 +5,69 @@ module StallCompiler{
         private prefix: string = "T";
         private suffix: number = 0;
         
+        //temps
+        public getCurrentTemp(): string {
+            return this.prefix + this.suffix.toString();
+        }
+        
+        public getNextTemp(): string {
+            this.suffix++;
+            return this.prefix + this.suffix.toString();
+        }
+        
+        //items
+        public getItems(): staticTItem[] {
+            return this.items;
+        }
+        
+        public getItemAtIndex(index: number): staticTItem {
+            return this.items[index];
+        }
+        
+        public addItem(item: staticTItem): void {
+            this.items.push(item);
+        }
+        
+        public findItemWithIdentifier(id: string): staticTItem {
+            for (var i = 0; i < this.items.length; i++) {
+                if (this.items[i].getId() === id) {
+                    return this.items[i];
+                }
+            }
+        }
+        
+        public incrementTemp() {
+            this.suffix++;
+        }
+        
+        //offset
+        public getOffset(): number {
+            return this.suffix;
+        }
+        
+        public getItemWithId(temp): staticTItem {
+            for (var i = 0; i < this.items.length; i++) {
+                if (this.items[i].getTemp() === temp) {
+                    return this.items[i];
+                }
+            }
+            return null;    
+        }
+        
+        //same method as jump table
+        public removeTempsInCodeTable(codeT: CodeT): void {
+            var regex = /^(T[0-9])/;
+            for (var i = 0; i < codeT.table.length; i++) {
+                var current = codeT.table[i];
+                if (current.match(regex)) {
+                    var item: staticTItem = this.getItemWithId(current.match(regex)[1]);
+                    console.log("current address");
+                    console.log(codeT.getCurrentAddr());
+                    codeT.addByteAtAddr((parseInt(item.getTemp()[1]) + codeT.getCurrentAddr() + 1).toString(16), i.toString());
+                    codeT.addByteAtAddr("00", (i + 1).toString());
+                }
+            }
+        }
 
     }
 
