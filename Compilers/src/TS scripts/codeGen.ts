b377150d60039de028b7fd0f022ef102a728d13b
barbara stall <barbara.stall1@marist.edu>
2019-12-11 13:26:29 -0500

    Initialized code gen variables.

--- Compilers/src/TS scripts/codeGen.ts
+++ Compilers/src/TS scripts/codeGen.ts
@@ -1,6 +1,22 @@
+
+///<reference path='scope.ts' />
+///<reference path='node.ts' />
+///<reference path='globals.ts' />
+///<reference path='utils.ts' />
+///<reference path='logger.ts' />
+
+///<reference path='codeT.ts' />
+///<reference path='staticT.ts' />
+///<reference path='jumpT.ts' />
+
 //main file for project 4
 module StallCompiler{
     export class CodeGen{
+        //initialize variables
+        private static codeT: CodeT;
+        private static staticT: staticT;
+        private static jumpT: jumpT;
+        private static jumpTCounter: number = 0;
 
     }
 }
